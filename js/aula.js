e24178650ce399bf43d872e17d6d244f253cdcb8
RHUAN MAGALHAES ASSOLA <rhuan.massola@senacsp.edu.br>
2025-04-07 20:04:22 -0300

    att menu responsivo

--- js/aula.js
+++ js/aula.js
@@ -1,17 +1,17 @@
-let nome = "Rhuan"
+let nome = "Rhuan";
 // alert(nome);
 console.log(nome);
 
 const x = 10;
-const array = ["a", "b", "c"]
-const objeto = { name: "Rhuan", age:23 } //JSON
+const array = ["a", "b", "c"];
+const objeto = { name: "Rhuan", age: 23 }; //JSON
 
 console.log(x);
 console.log(typeof x);
-console.log('------------------');
+console.log("------------------");
 console.log(array);
 console.log(typeof array);
-console.log('------------------');
+console.log("------------------");
 console.log(objeto);
 console.log(typeof objeto);
 console.log(objeto.name);
@@ -23,27 +23,27 @@ condicional
 ....
 */
 
-if (2 > 1){
-    console.log("2 é maior que 1")
+if (2 > 1) {
+  console.log("2 é maior que 1");
 } else {
-    console.log("2 não é maior que 1")
+  console.log("2 não é maior que 1");
 }
- // Estrutra de repetição
- // i = i + 1
- const arr = ["amora", "banana", "caju", "Maracujá", "Melão"];
- for (let i = 0; i < arr.length; i++){
-    // console.log('O valor da repetição é '+ arr[i]);
-    // template literal, é o uso da crase `
-    console.log(`O valor da repetição é ${arr[i]}`);
- }
+// Estrutra de repetição
+// i = i + 1
+const arr = ["amora", "banana", "caju", "damasco", "framboesa"];
+for (let i = 0; i < arr.length; i++) {
+  // console.log('O valor da repetição é '+ arr[i]);
+  // template literal, é o uso da crase `
+  console.log(`O valor da repetição é ${arr[i]}`);
+}
 
- //função
- function soma(){
-    console.log(22+11);
- }
- soma();
+//função
+function soma() {
+  console.log(22 + 11);
+}
+soma();
 
- function calcula(a,b){
-    console.log(a*b)
- }
-calcula(12,12);
+function calcula(a, b) {
+  console.log(a * b);
+}
+calcula(12, 12);
--- js/scripts.js
+++ js/scripts.js
@@ -1,83 +1,53 @@
-// selecionar os elementos dos slides
-const slides = document.querySelectorAll(".banner");
-//
-const dots = document.querySelectorAll(".dot");
+// Selecionando elementos do menu Mobile
+const menuBtn = document.querySelector("#menu");
+const closemenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+//fechar o menu, após clicar na opção sobre
+const about = document.querySelector('#mobile-navbar [href="#about"]');
+
+
+// Selecionar os elementos dos slides
+const slides = document.querySelectorAll(".banner"); // Retorna um NodeList
+const dots = document.querySelectorAll(".dot"); // Retorna um NodeList
 // variavel de controle
-let slideIndex = 0;
+let slideIndex = 1;
 
-function showSlides(){
-    for(let i=0; i < slides.length; i++){
-        slides[i].classList.remove("active")
-        dots[i].classList.remove("active")
-    }
+function showSlides() {
+  // console.log(slides)
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    dots[i].classList.remove("active");
+  }
 
-    slideIndex++; // avançando a variavel
-    if (slideIndex > slides.length){
-        slideIndex = 1;
-    }
-    console.log(slideIndex)
-    console.log(slides[slideIndex -1])
-    slides[slideIndex -1].classList.add("active")
-    dots[slideIndex -1].classList.add("active")
+  slideIndex++; // Incrementa o slideIndex
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
 
-    setTimeout(showSlides, 3000)
+  slides[slideIndex - 1].classList.add("active");
+  dots[slideIndex - 1].classList.add("active");
 
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
-//Inicialização
-showSlides()
+// Ativar menu mobile
+// menuBtn.addEventListener("click", (e) => {
+//   menu.classList.add("menu-active");
+// });
 
+// closemenuBtn.addEventListener("click", (e) => {
+//   menu.classList.remove("menu-active");
+// });
 
+[menuBtn, closemenuBtn].forEach(  (btn) => {
+    btn.addEventListener("click", (e) =>{
+        menu.classList.toggle("menu-active")
+    })
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// DOM
-// Navegar no documento html
-// const brand = document.querySelector("#brand");
-// console.log(brand)
-// brand.addEventListener('click', function(event){
-//     // console.log("testando o click");
-//     // console.log(event.target)
-//     event.target.style.color ="blue";
-// })
+about.addEventListener("click", (e) => {
+    menu.classList.remove("menu-active");
+  });
+// Inicialização do slides
+showSlides();
